3baa5f05c7ecd9e670eae1b4addc20d4c919b48b
Samson Carter <129422671+SamsonCart@users.noreply.github.com>
2024-12-07 13:30:51 -0500

    updated tracing.js

--- tracing.js
+++ tracing.js
@@ -1,28 +1,32 @@
+Heres the one I used with a peer that helped me work out
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+// Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-
+// Exporter
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 module.exports = (serviceName) => {
+    // Configure the Jaeger Exporter
     const exporter = new JaegerExporter({
-        endpoint: "http://localhost:14268/api/traces",
+        endpoint: "http://localhost:14268/api/traces", // Default Jaeger endpoint
+        serviceName: serviceName,
     });
-
+    // Create a provider and register the exporter
     const provider = new NodeTracerProvider({
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
     });
-
+    // Add the exporter to the span processor
     provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
     provider.register();
-
+    // Register instrumentations
     registerInstrumentations({
         instrumentations: [
             new HttpInstrumentation(),
@@ -31,6 +35,5 @@ module.exports = (serviceName) => {
         ],
         tracerProvider: provider,
     });
-
     return trace.getTracer(serviceName);
 };
